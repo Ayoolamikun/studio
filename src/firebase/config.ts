b4fbe37545f164ec8de535c6e67d414d8284ff85
Ayoolamikun Awodeyi <awodeyiayoola@gmail.com>
2025-11-08 13:57:19 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7087913639-9a972",
+  "appId": "1:833191446723:web:d8497cf4d7b502c718fa76",
+  "apiKey": "AIzaSyB1Me1GP5hIQiHEUaLqC7nWfv-EMdejFic",
+  "authDomain": "studio-7087913639-9a972.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "833191446723"
+};
