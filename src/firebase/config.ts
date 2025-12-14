c8666564d03860fb2b9a2130cdc0e1003ccb1a76
Your Name <awodeyiayoola@gmail.com>
2025-12-14 07:33:41 +0000

    const firebaseConfig = {   apiKey: "your-api-key",   authDomain: "your-a

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:833191446723:web:d8497cf4d7b502c718fa76",
   "apiKey": "AIzaSyB1Me1GP5hIQiHEUaLqC7nWfv-EMdejFic",
   "authDomain": "studio-7087913639-9a972.firebaseapp.com",
+  "storageBucket": "studio-7087913639-9a972.appspot.com",
   "measurementId": "",
   "messagingSenderId": "833191446723"
 };
